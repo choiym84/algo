dc1d1b9621926c60e11bc18c9ef33df4182bfa22
choiym84 <131252323+choiym84@users.noreply.github.com>
2026-03-16 17:21:20 +0900

    [Gold III] Title: 파티, Time: 196 ms, Memory: 21072 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -1,88 +1,112 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		// ------여기에 솔루션 코드를 작성하세요.------------
 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int INF = 1_000_000_000;
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-		int INF = 1_000_000_000;
+		int X = Integer.parseInt(st.nextToken()) - 1;
 
-		int map[][] = new int[N + 1][N + 1];
-		for (int i = 1; i <= N; i++) {
-			Arrays.fill(map[i], INF);
-		}
+		int[][] adj = new int[N][N];
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()) - 1;
+			int b = Integer.parseInt(st.nextToken()) - 1;
 			int w = Integer.parseInt(st.nextToken());
 
-			map[a][b] = w;
-
+			adj[a][b] = w;
 		}
 
-		int vertex1[] = new int[N + 1];
-		Arrays.fill(vertex1, INF);
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-		pq.add(new int[] { X, 0 });
-		vertex1[X] = 0;
+		int dist1[] = new int[N];
+		boolean[] visited = new boolean[N];
+		Arrays.fill(dist1, INF);
+
+		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+			return a[1] - b[1];
+		});
+
+		pq.offer(new int[] { X, 0 });
+		dist1[X] = 0;
+
 		while (!pq.isEmpty()) {
 
-			int out[] = pq.poll();
-			int v = out[0];
-			int w = out[1];
+			int[] n = pq.poll();
+			int to = n[0];
+			int w = n[1];
 
-			for (int i = 1; i <= N; i++) {
+			if (visited[to])
+				continue;
 
-				if (vertex1[i] <= vertex1[v] + map[v][i])
+			visited[to] = true;
+
+			for (int i = 0; i < N; i++) {
+
+				if (visited[i])
 					continue;
-				pq.add(new int[] { i, map[v][i] });
-				vertex1[i] = vertex1[v] + map[v][i];
+				if (adj[to][i] == 0)
+					continue;
+				if (dist1[i] > dist1[to] + adj[to][i]) {
+					dist1[i] = dist1[to] + adj[to][i];
+					pq.offer(new int[] { i, dist1[i] });
+				}
 
 			}
 		}
-		
-		
-		int vertex2[] = new int[N + 1];
-		Arrays.fill(vertex2, INF);
-		pq.add(new int[] { X, 0 });
-		vertex2[X] = 0;
+
+		int dist2[] = new int[N];
+		visited = new boolean[N];
+		Arrays.fill(dist2, INF);
+
+		pq.offer(new int[] { X, 0 });
+		dist2[X] = 0;
+
 		while (!pq.isEmpty()) {
 
-			int out[] = pq.poll();
-			int v = out[0];
-			int w = out[1];
+			int[] n = pq.poll();
+			int to = n[0];
+			int w = n[1];
 
-			for (int i = 1; i <= N; i++) {
+			if (visited[to])
+				continue;
 
-				if (vertex2[i] <= vertex2[v] + map[i][v])
+			visited[to] = true;
+
+			for (int i = 0; i < N; i++) {
+
+				if (visited[i])
 					continue;
-				pq.add(new int[] { i, map[i][v] });
-				vertex2[i] = vertex2[v] + map[i][v];
+				if (adj[i][to] == 0)
+					continue;
+				if (dist2[i] > dist2[to] + adj[i][to]) {
+					dist2[i] = dist2[to] + adj[i][to];
+					pq.offer(new int[] { i, dist2[i] });
+				}
 
 			}
 		}
 
 		int max = 0;
-		for(int i = 1;i <= N;i++) {
-			max = Math.max(max,vertex1[i]+vertex2[i]);
+		for (int i = 0; i < N; i++) {
+
+			max = Math.max(max, dist1[i] + dist2[i]);
+
 		}
-		
+
 		System.out.println(max);
 
 	}
